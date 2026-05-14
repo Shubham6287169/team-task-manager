0b2df6cdcac0cc6499b9e4a42681ac9637d1e7ee
Shubham <skvatas2684@gmail.com>
2026-05-14 20:09:09 +0530

    intial commit

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ async def serve_spa(full_path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
